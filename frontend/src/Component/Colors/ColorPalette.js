27c382be3a55f6261d5d1fbf8e79d88590ca0c09
Staley <ryan.staley@accontrols.com>
2019-06-19 07:42:30 -0400

    refactoring light / dark background check

--- frontend/src/Component/Colors/ColorPalette.js
+++ frontend/src/Component/Colors/ColorPalette.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import lightOrDark from'./Helper';
 
+
+function blackOrWhite(color) {
+  const hue = lightOrDark(color)
+
+  if (hue === 'light') {
+    return '#000000'
+  } else {
+    return '#ffffff'
+  }
+}
+
+
 function ColorPalette(props) {
     const name = "color_box";
     const colors = props.colors
     const colorBoxes = colors.map((color) => {
-      let fontColor
-      let brightness = lightOrDark(color)
-      if (brightness === 'light') {
-        fontColor = '#000000';
-      } else {
-        fontColor = '#ffffff';
-      }
-      return (
-      <div className={name} style={{backgroundColor: color}}>
-        <div className="color_box__labels" style={{color: fontColor}}>{color}</div>
-      </div>
-      )
+        let fontColor = blackOrWhite(color)
+        return (
+          <div className={name} style={{backgroundColor: color}}>
+            <div className="color_box__labels" style={{color: fontColor}}>{color}</div>
+          </div>
+        )
       }
     )
     return (
