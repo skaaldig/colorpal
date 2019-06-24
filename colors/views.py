f1e9bdcccded84122673979df92c43fe18b4ab56
Staley <ryan.staley@accontrols.com>
2019-06-24 15:41:56 -0400

    only allow post requests in api views

--- colors/views.py
+++ colors/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework import viewsets
 
 from .models import ColorPalette, Image
@@ -15,8 +15,17 @@ class ColorPaletteViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows color palettes to be viewed or edited
     """
+    permission_classes = (IsAuthenticated,)
     queryset = ColorPalette.objects.all()
     serializer_class = ColorPaletteSerializer
+
+    def get_permissions(self):
+        if self.action == 'create':
+            permission_classes = [AllowAny]
+        else: 
+            permission_classes = [IsAuthenticated]
+        
+        return [permission() for permission in permission_classes]
 
 
 class ImageViewSet(viewsets.ModelViewSet):
@@ -26,6 +35,16 @@ class ImageViewSet(viewsets.ModelViewSet):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
     lookup_field = 'uuid'
+
+    def get_permissions(self):
+        if self.action == 'create':
+            permission_classes = [AllowAny]
+        else: 
+            permission_classes = [IsAuthenticated]
+        
+        return [permission() for permission in permission_classes]
+        
+
 
 
 
--- frontend/src/Component/Colors/Instructions.js
+++ frontend/src/Component/Colors/Instructions.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSpring, animated } from 'react-spring';
 
 function Instructions(props) {
-    const slideIn = useSpring({from: {marginLeft: -500}, to: {marginLeft: 0}})
+    const slideIn = useSpring({from: {marginLeft: -700}, to: {marginLeft: 0}})
     return (
     <div className="instructions">
         <animated.div className="instructions__wrapper" style={slideIn}>
@@ -11,9 +11,6 @@ function Instructions(props) {
             </h1>
             <p>
                 Upload a photo to discover the most dominant colors in the image and their hexidecimal codes.
-            </p>
-            <p>
-                File size is limited to 3MB
             </p>
         </animated.div>
     </div>
